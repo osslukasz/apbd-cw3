d3c8db7e3b5a288f91000586e32c676d6f8e27d6
Lukasz Ossowski <s33627@pjwstk.edu.pl>
2026-03-26 00:59:10 +0100

    fixed redundant variable

--- APBD3/Laptop.cs
+++ APBD3/Laptop.cs
@@ -4,7 +4,6 @@ public class Laptop : Sprzet
 {
     public string CPU { get; set; }
     public string GPU { get; set; }
-    public string Nazwa { get; set; }
 
     public Laptop(string nazwa, string cpu, string gpu) : base(nazwa)
     {
