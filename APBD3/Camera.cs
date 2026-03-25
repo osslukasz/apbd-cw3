51c3bfb9b83e94e2333c0b4ef590a929d5d7a13b
Lukasz Ossowski <s33627@pjwstk.edu.pl>
2026-03-26 00:58:03 +0100

    Testinf different camera class constructor

--- APBD3/Camera.cs
+++ APBD3/Camera.cs
@@ -4,10 +4,5 @@ public class Camera: Sprzet
 {
     public string Type { set; get; }
     public string Resolution { set; get; }
-
-    public Camera(string nazwa, string type, string resolution) : base(nazwa)
-    {
-        this.Type = type;
-        this.Resolution = resolution;
-    }
+    
 }
--- APBD3/Sprzet.cs
+++ APBD3/Sprzet.cs
@@ -2,9 +2,14 @@ namespace APBD3;
 
 public abstract class Sprzet
 {
-    public Guid Id { get; } =  Guid.NewGuid();
+    public string Id { get; } =  Guid.NewGuid().ToString().Substring(0, 8);
     public string Nazwa { get; set; }
     public bool Dostepny { get; set; } = true;
 
     protected Sprzet(string nazwa) => Nazwa = nazwa;
+
+    protected Sprzet()
+    {
+        throw new NotImplementedException();
+    }
 }
